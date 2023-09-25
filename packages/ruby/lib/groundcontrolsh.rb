7c863bdf1167d293bba9dc18d638c54f5b349d4f
Alberto Gimeno <gimenete@gmail.com>
2023-09-25 15:07:25 +0200

    fix: implement overrides for Ruby

--- packages/ruby/lib/groundcontrolsh.rb
+++ packages/ruby/lib/groundcontrolsh.rb
@@ -11,20 +11,34 @@ class GroundControl
     @base_url = options[:base_url] || 'https://api.groundcontrol.sh'
     @ttl = options[:cache] || nil
     @cache = {}
+
+    @_actor_overrides = {}
+    @_flag_overrides = {}
+    @full_override = nil
   end
 
-  def feature_flag_enabled?(feature_name, options = {})
+  def feature_flag_enabled?(flag_name, options = {})
     actors = options[:actors] || []
 
-    query = actors.map { |actor| "actorIds=#{CGI.escape(actor)}" }
+    actor_overrides = @_actor_overrides[flag_name]
+    actors.each do |actor_id|
+      override = actor_overrides[actor_id]
+      return override if override != nil
+    end if actor_overrides != nil && actors != nil
 
-    url = URI("#{@base_url}/projects/#{@project_id}/flags/#{feature_name}/check?#{query.join('&')}")
+    flag_override = @_flag_overrides[flag_name]
+    return flag_override if flag_override != nil
+    return @_full_override if @_full_override != nil
+
+    query = actors.map { |actor| "actorIds=#{CGI.escape(actor)}" }
+    url = URI("#{@base_url}/projects/#{@project_id}/flags/#{flag_name}/check?#{query.join('&')}")
     cached = @cache[url]
     if cached
       if cached[:expires] < Time.now.to_i
         @cache.delete(url)
+      else
+        return cached[:enabled]
       end
-      return cached[:enabled]
     end
 
     http = Net::HTTP.new(url.host, url.port)
@@ -44,5 +58,43 @@ class GroundControl
     end
 
     return enabled
+  end
+
+  def disable_feature_flag(flag_name, options = nil)
+    set_feature_flag_enabled(false, flag_name, options)
+  end
+
+  def disable_all_feature_flags
+    @_full_override = false
+  end
+
+  def enable_feature_flag(flag_name, options = nil)
+    set_feature_flag_enabled(true, flag_name, options)
+  end
+
+  def enable_all_feature_flags()
+    @_full_override = true
+  end
+
+  def reset
+    @_actor_overrides.clear
+    @_flag_overrides.clear
+    @_full_override = nil
+  end
+
+  private
+
+  def set_feature_flag_enabled(enabled, flag_name, options)
+    options = {} if options == nil
+    actors_ops = options[:actors]
+    if actors_ops == nil
+      @_flag_overrides[flag_name] = enabled
+    else
+      actors = @_actor_overrides[flag_name] || {}
+      actors_ops.each do |actor_id|
+        actors[actor_id] = enabled
+      end
+      @_actor_overrides[flag_name] = actors
+    end
   end
 end
--- packages/ruby/test/groundcontrol_test.rb
+++ packages/ruby/test/groundcontrol_test.rb
@@ -6,9 +6,11 @@ include WebMock::API
 WebMock.enable!
 
 class TestGroundControl < Test::Unit::TestCase
-  @api_key = "gcp_#{Time.now.to_i}"
-  @project_id = "P#{Time.now.to_i}"
-  @flag_name = "flag-name-#{Time.now.to_i}"
+  def setup
+    @api_key = "gcp_#{Time.now.to_i}"
+    @project_id = "P#{Time.now.to_i}"
+    @flag_name = "flag-name-#{Time.now.to_i}"
+  end
 
   def test_simple
     stub_request(:get, "https://api.groundcontrol.sh/projects/#{@project_id}/flags/#{@flag_name}/check").
@@ -52,9 +54,9 @@ class TestGroundControl < Test::Unit::TestCase
       }).
     to_return(status: 200, body: { enabled: false }.to_json, headers: {})
 
-    # The returned value is still true because it's been cached
+    # The returned value is fresh because the cached value has expired
     enabled = client.feature_flag_enabled?(@flag_name)
-    assert_equal(true, enabled)
+    assert_equal(false, enabled)
   end
 
   def test_with_ttl_cache_fail
@@ -74,17 +76,82 @@ class TestGroundControl < Test::Unit::TestCase
     enabled = client.feature_flag_enabled?(@flag_name)
     assert_equal(true, enabled)
 
-    # Stub an enabled: false request
-    stub_request(:get, "https://api.groundcontrol.sh/projects/#{@project_id}/flags/#{@flag_name}/check").
-    with(
-      headers: {
-          'Authorization'=>"Bearer #{@api_key}",
-      }).
-    to_return(status: 200, body: { enabled: false }.to_json, headers: {})
+    WebMock.reset!
+    WebMock.disable_net_connect!(allow_localhost: true)
 
-    # The returned value is fresh because the cached value has expired
+    # The returned value is still true because it's been cached
     enabled = client.feature_flag_enabled?(@flag_name)
     assert_equal(true, enabled)
+    assert_not_requested(:any, "https://api.groundcontrol.sh")
+  end
+
+  def test_overrides
+    WebMock.disable_net_connect!(allow_localhost: true)
+
+    client = GroundControl.new(
+      project_id: @project_id,
+      api_key: @api_key,
+    )
+
+    client.disable_all_feature_flags
+    assert_equal(false, client.feature_flag_enabled?(@flag_name))
+    client.reset
+
+    # enable all flags
+    client.enable_all_feature_flags
+    assert_equal(true, client.feature_flag_enabled?(@flag_name))
+    client.reset
+
+    # disable a flag for all actors
+    client.disable_feature_flag(@flag_name)
+    assert_equal(false, client.feature_flag_enabled?(@flag_name))
+    client.reset
+
+    # enable a flag for all actors
+    client.enable_feature_flag(@flag_name)
+    assert_equal(true, client.feature_flag_enabled?(@flag_name))
+    client.reset
+
+    # disable a flag for a specific actor
+    client.disable_feature_flag(@flag_name, { actors: ["user1"] })
+    assert_equal(false,
+      client.feature_flag_enabled?(@flag_name, { actors: ["user1"] })
+    )
+    client.reset
+
+    # enable a flag for a specific actor
+    client.enable_feature_flag(@flag_name, { actors: ["user1"] })
+    assert_equal(true,
+      client.feature_flag_enabled?(@flag_name, { actors: ["user1"] })
+    )
+    client.reset
+
+    # overrides at the flag level take precedence over full overrides
+    client.disable_all_feature_flags
+    client.enable_feature_flag(@flag_name)
+    assert_equal(true, client.feature_flag_enabled?(@flag_name))
+    client.reset
+
+    # overrides at the actor level take precedence over other overrides
+    client.disable_all_feature_flags
+    client.disable_feature_flag(@flag_name)
+    client.enable_feature_flag(@flag_name, { actors: ["user1"] })
+    assert_equal(true,
+      client.feature_flag_enabled?(@flag_name, { actors: ["user1"] })
+    )
+    client.reset
+
+    # actor overrides work for the same and different actors
+    client.enable_feature_flag(@flag_name, { actors: ["user1"] })
+    client.disable_feature_flag(@flag_name, { actors: ["user1"] })
+    client.enable_feature_flag(@flag_name, { actors: ["user2"] })
+    assert_equal(false,
+      client.feature_flag_enabled?(@flag_name, { actors: ["user1"] })
+    )
+    assert_equal(true,
+      client.feature_flag_enabled?(@flag_name, { actors: ["user2"] })
+    )
+    client.reset
   end
 
 end
